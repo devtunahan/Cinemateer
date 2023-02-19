3150a58571ace85d815e7dba769ccef38985d2c7
tunahan <bstunahan@gmail.com>
2023-02-19 23:28:14 +0100

    Initial commit with mvc in maven

---
+++ src/main/java/com/tunahan/cinemateer/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.tunahan.cinemateer.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.tunahan.cinemateer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    exports com.tunahan.cinemateer.model;
+    opens com.tunahan.cinemateer.model to javafx.fxml;
+
+    exports com.tunahan.cinemateer.controller to javafx.fxml;
+    opens com.tunahan.cinemateer.controller to javafx.fxml;
+
+
+}
